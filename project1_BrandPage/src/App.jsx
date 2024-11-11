7c30367698f99ea4ee82b42566e5f65853994020
N4G4R4J <n4g4r4j@localhost.localdomain>
2024-11-12 00:45:08 +0530

    react basic project

---
+++ project1_BrandPage/src/App.jsx
@@ -0,0 +1,17 @@
+import Navigation from "./components/Navigation";
+import HeroSection from "./components/Hero";
+import "./App.css";
+
+
+const App = () => {
+
+  return (
+    <div>
+      <Navigation/>
+      <HeroSection/>
+    </div>
+  )
+};
+
+
+export default App;
