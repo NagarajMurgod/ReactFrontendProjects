d95c365475d9aa87f51af31bc91f7c314326e49f
Nagaraj Murgod <01fe19bee418@gmail.com>
2024-11-26 21:33:59 +0530

    completed project4

--- Project4_PortFolioPage/Portfolio/src/App.jsx
+++ Project4_PortFolioPage/Portfolio/src/App.jsx
@@ -1,6 +1,10 @@
 import styles from "./App.module.css"
-import { Hero } from "./components/Hero/Hero"
-import { Navbar } from "./components/Navbar/Navbar"
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
 
 
 function App() {
@@ -11,6 +15,10 @@ function App() {
       <div className={styles.App}>
         <Navbar/>
         <Hero/>
+        <About/>
+        <Experience/>
+        <Projects />
+        <Contact />
       </div>
     </>
   )
